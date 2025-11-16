d2ab381d0ffbc03ff391e90f912c6db0eeb9a9fd
Bruhtek <github@bruhtek.com>
2025-11-16 21:11:02 +0100

    Offline - palindrome

---
+++ offline/palindrom.py
@@ -0,0 +1,58 @@
+def is_palindrome(text: str)->bool:
+    n = len(text)
+    for i in range(n//2 + 1):
+        if text[i] != text[n-i-1]:
+            return False
+    return True
+
+PALINDROME_MIN_SIZE=5
+
+def find_palindrome(verse: str)->str|None:
+    n = len(verse)
+    for i in range(n-4):
+        for j in range(i+4, n):
+            subVerse = verse[i:j+1]
+            if is_palindrome(subVerse):
+                return subVerse
+    return None
+
+def find_all_crosses(tab: list[str])->list[str]:
+    n = len(tab)
+    res = []
+    for i in range(n):
+        cross = ""
+        j = 0
+        while i < n and j < n:
+            cross += tab[i][j]
+            j += 1
+            i += 1
+        if len(cross) >= 5:
+            res.append(cross)
+    for j in range(1, n):
+        cross = ""
+        i = 0
+        while i < n and j < n:
+            cross += tab[i][j]
+            j += 1
+            i += 1
+        if len(cross) >= 5:
+            res.append(cross)
+
+    return res
+
+def main()->str:
+    n = int(input())
+    tab = [input() for _ in range(n)]
+    verses = find_all_crosses(tab) + tab + [''.join([tab[i][j] for i in range(n)]) for j in range(n)]
+    palindromes = {}
+    for verse in verses:
+        pali = find_palindrome(verse)
+        if pali is not None:
+            if pali in palindromes:
+                return pali
+            else:
+                palindromes[pali] = 1
+    return "NONE"
+
+if __name__ == "__main__":
+    print(main())
